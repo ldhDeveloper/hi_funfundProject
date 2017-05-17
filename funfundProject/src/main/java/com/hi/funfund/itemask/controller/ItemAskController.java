a6e7e65b787bd6f7665d665bf1e1de1a83a4da9b
user1 <user1@KH_C>
2017-05-17 15:20:20 +0900

    홍쥬 댓글 페이지 따로 작성 등록이 안돼요 pro_no보내야하는데..

--- funfundProject/src/main/java/com/hi/funfund/itemask/controller/ItemAskController.java
+++ funfundProject/src/main/java/com/hi/funfund/itemask/controller/ItemAskController.java
@@ -1,6 +1,5 @@
 package com.hi.funfund.itemask.controller;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
@@ -9,37 +8,33 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.hi.funfund.account.model.vo.Account;
-import com.hi.funfund.fundmenu.model.vo.FundMenu;
-import com.hi.funfund.item.model.vo.Item;
 import com.hi.funfund.itemask.model.service.ItemAskService;
 import com.hi.funfund.itemask.model.vo.ItemAsk;
 
 @Controller
-@RequestMapping("itemAsk")
+
 public class ItemAskController {
 
 	@Autowired
 	private ItemAskService itemAskService;
 
-	@RequestMapping(value="/comment.ask")
-	public ModelAndView insert(HttpSession session, HttpServletRequest request,
-			ModelAndView model) {
+	@RequestMapping("coinsert.ask")
+	public ModelAndView insert(HttpSession session, HttpServletRequest request, ModelAndView model) {
+		String pro_no =request.getParameter("pro_no");
 		String acontent = request.getParameter("acontent");
 		Account account = (Account) session.getAttribute("account");
-		String pro_no = request.getParameter("pro_no");
+		System.out.println(account);
+		System.out.println(acontent);
+		System.out.println(pro_no);
 		HashMap<String, String> cmap = new HashMap<String, String>();
 		cmap.put("pro_no", pro_no);
 		cmap.put("acontent", acontent);
 		cmap.put("id_no", Integer.toString(account.getAno()));
-		int result = itemAskService.insert(cmap);
-		model.setViewName("funding/detailList");
+		int result = itemAskService.insert(cmap);		
 		return model;
 	}
 
@@ -54,12 +49,13 @@ public class ItemAskController {
 		return null;
 	}
 
-	public String selectList(int pro_no, ModelAndView model) {
-
-		List<ItemAsk> itemAskList = itemAskService.selectList(pro_no);
-		model.addObject("itemAskList", itemAskList);
-
-		return null;
+	@RequestMapping("reply.ask")
+	public ModelAndView selectList(ModelAndView model,HttpServletRequest request) {
+		int pro_no = Integer.parseInt(request.getParameter("pro_no"));
+		List<ItemAsk> aList = itemAskService.selectList(pro_no);
+		model.addObject("aList", aList);
+		model.setViewName("funding/reply");
+		return model;
 	}
 
 }
