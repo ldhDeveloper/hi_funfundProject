aa7389cd2665f8ef32b03fdbd249990cb47b80f4
purpleapple <tjdvkf19@gmail.com>
2017-05-25 15:14:17 +0900

    챗봇

---
+++ funfundProject/src/main/java/com/hi/funfund/chatBot/ChatBotController.java
@@ -0,0 +1,13 @@
+package com.hi.funfund.chatBot;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+public class ChatBotController {
+
+	@RequestMapping("keyboard")
+	public void keyboard(ModelAndView model){
+		
+		
+	}
+}
