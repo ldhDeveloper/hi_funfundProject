a348f79540bf1a95c0618e41026e68f07b2ac058
jinho <curryfavour9@gmail.com>
2017-05-29 15:18:42 +0900

    sdafasdf

--- funfundProject/src/main/java/com/hi/funfund/fundmenu/model/vo/FundMenu.java
+++ funfundProject/src/main/java/com/hi/funfund/fundmenu/model/vo/FundMenu.java
@@ -18,17 +18,13 @@ public class FundMenu implements Serializable {
 	private Integer mcount;
 	private String delyn;
 	private String dcost;
-
+	private Integer fundcount;
+	private int remain;
 	
-	
-	public FundMenu() {
-	}
-
-	
-	
+	public FundMenu(){}
 	
 	public FundMenu(int mno, String mname, Integer mcost, int pro_no, String mcontent, Date mdate, Integer mnum,
-			Integer mcount, String delyn, String dcost) {
+			Integer mcount, String delyn, String dcost, Integer fundcount, int remain) {
 		super();
 		this.mno = mno;
 		this.mname = mname;
@@ -40,119 +36,89 @@ public class FundMenu implements Serializable {
 		this.mcount = mcount;
 		this.delyn = delyn;
 		this.dcost = dcost;
+		this.fundcount = fundcount;
+		this.remain = remain;
 	}
-
-
-
-
 	public int getMno() {
 		return mno;
 	}
-
-
-	public void setMno(int mno) {
-		this.mno = mno;
-	}
-
-
 	public String getMname() {
 		return mname;
 	}
-
-
-	public void setMname(String mname) {
-		this.mname = mname;
-	}
-
-
 	public Integer getMcost() {
 		return mcost;
 	}
-
-
-	public void setMcost(Integer mcost) {
-		this.mcost = mcost;
-	}
-
-
 	public int getPro_no() {
 		return pro_no;
 	}
-
-
-	public void setPro_no(int pro_no) {
-		this.pro_no = pro_no;
-	}
-
-
 	public String getMcontent() {
 		return mcontent;
 	}
-
-
-	public void setMcontent(String mcontent) {
-		this.mcontent = mcontent;
-	}
-
-
 	public Date getMdate() {
 		return mdate;
 	}
-
-
-	public void setMdate(Date mdate) {
-		this.mdate = mdate;
-	}
-
-
 	public Integer getMnum() {
 		return mnum;
 	}
-
-
-	public void setMnum(Integer mnum) {
-		this.mnum = mnum;
-	}
-
-
 	public Integer getMcount() {
 		return mcount;
 	}
-
-
-	public void setMcount(Integer mcount) {
-		this.mcount = mcount;
-	}
-
-
 	public String getDelyn() {
 		return delyn;
 	}
-
-
-	public void setDelyn(String delyn) {
-		this.delyn = delyn;
-	}
-
-
 	public String getDcost() {
 		return dcost;
 	}
-
-
+	public Integer getFundcount() {
+		return fundcount;
+	}
+	public int getRemain() {
+		return remain;
+	}
+	public void setMno(int mno) {
+		this.mno = mno;
+	}
+	public void setMname(String mname) {
+		this.mname = mname;
+	}
+	public void setMcost(Integer mcost) {
+		this.mcost = mcost;
+	}
+	public void setPro_no(int pro_no) {
+		this.pro_no = pro_no;
+	}
+	public void setMcontent(String mcontent) {
+		this.mcontent = mcontent;
+	}
+	public void setMdate(Date mdate) {
+		this.mdate = mdate;
+	}
+	public void setMnum(Integer mnum) {
+		this.mnum = mnum;
+	}
+	public void setMcount(Integer mcount) {
+		this.mcount = mcount;
+	}
+	public void setDelyn(String delyn) {
+		this.delyn = delyn;
+	}
 	public void setDcost(String dcost) {
 		this.dcost = dcost;
 	}
-
-
-
-
+	public void setFundcount(Integer fundcount) {
+		this.fundcount = fundcount;
+	}
+	public void setRemain(int remain) {
+		this.remain = remain;
+	}
 	@Override
 	public String toString() {
 		return "FundMenu [mno=" + mno + ", mname=" + mname + ", mcost=" + mcost + ", pro_no=" + pro_no + ", mcontent="
 				+ mcontent + ", mdate=" + mdate + ", mnum=" + mnum + ", mcount=" + mcount + ", delyn=" + delyn
-				+ ", dcost=" + dcost + "]";
+				+ ", dcost=" + dcost + ", fundcount=" + fundcount + ", remain=" + remain + "]";
 	}
+	
+	
 
 
 	
