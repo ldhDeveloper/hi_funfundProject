98518b33b9ecab4e63f9d0c092cf9d8ad7a4030c
ldh8206 <ldh8206@gmail.com>
2017-05-30 15:06:29 +0900

    admin start!!!

--- funfundProject/src/main/java/com/hi/funfund/item/model/vo/Item.java
+++ funfundProject/src/main/java/com/hi/funfund/item/model/vo/Item.java
@@ -40,7 +40,13 @@ public class Item implements Serializable {
 	private Integer repcount;
 	private String thumbnail;
 	private String pimage;
-
+	private String billstatus;
+	private Date firstdeposit;
+	private Date seconddeposit;
+	private Integer firstprice;
+	private Integer secondprice;
+	private String icomment;
+	
 	public Item() {
 	}
 
@@ -48,7 +54,8 @@ public class Item implements Serializable {
 			String s_psdate, Date pedate, String s_pedate, String pshort, Integer ecost, String refund, String pvideo,
 			String pstatus, int likecount, String sharelink, String bankcode, String accpnm, String accnum,
 			String cname, String cs_email, String cs_phone, Integer fundamount, Integer fundcount, Integer supportcount,
-			Integer upcount, Integer repcount, String thumbnail, String pimage) {
+			Integer upcount, Integer repcount, String thumbnail, String pimage, String billstatus, Date firstdeposit,
+			Date seconddeposit, Integer firstprice, Integer secondprice, String icomment) {
 		super();
 		this.pro_no = pro_no;
 		this.ano = ano;
@@ -79,6 +86,62 @@ public class Item implements Serializable {
 		this.repcount = repcount;
 		this.thumbnail = thumbnail;
 		this.pimage = pimage;
+		this.billstatus = billstatus;
+		this.firstdeposit = firstdeposit;
+		this.seconddeposit = seconddeposit;
+		this.firstprice = firstprice;
+		this.secondprice = secondprice;
+		this.icomment = icomment;
+	}
+
+	
+
+	public String getBillstatus() {
+		return billstatus;
+	}
+
+	public void setBillstatus(String billstatus) {
+		this.billstatus = billstatus;
+	}
+
+	public Date getFirstdeposit() {
+		return firstdeposit;
+	}
+
+	public void setFirstdeposit(Date firstdeposit) {
+		this.firstdeposit = firstdeposit;
+	}
+
+	public Date getSeconddeposit() {
+		return seconddeposit;
+	}
+
+	public void setSeconddeposit(Date seconddeposit) {
+		this.seconddeposit = seconddeposit;
+	}
+
+	public Integer getFirstprice() {
+		return firstprice;
+	}
+
+	public void setFirstprice(Integer firstprice) {
+		this.firstprice = firstprice;
+	}
+
+	public Integer getSecondprice() {
+		return secondprice;
+	}
+
+	public void setSecondprice(Integer secondprice) {
+		this.secondprice = secondprice;
+	}
+
+	public String getIcomment() {
+		return icomment;
+	}
+
+	public void setIcomment(String icomment) {
+		this.icomment = icomment;
 	}
 
 	public Integer getPro_no() {
@@ -322,11 +385,9 @@ public class Item implements Serializable {
 				+ bankcode + ", accpnm=" + accpnm + ", accnum=" + accnum + ", cname=" + cname + ", cs_email=" + cs_email
 				+ ", cs_phone=" + cs_phone + ", fundamount=" + fundamount + ", fundcount=" + fundcount
 				+ ", supportcount=" + supportcount + ", upcount=" + upcount + ", repcount=" + repcount + ", thumbnail="
-				+ thumbnail + ", pimage=" + pimage + "]";
-	};
-	
-	
-	
-	
+				+ thumbnail + ", pimage=" + pimage + ", billstatus=" + billstatus + ", firstdeposit=" + firstdeposit
+				+ ", seconddeposit=" + seconddeposit + ", firstprice=" + firstprice + ", secondprice=" + secondprice
+				+ ", icomment=" + icomment + "]";
+	}
 	
 }
---
+++ funfundProject/src/main/webapp/WEB-INF/views/admin/itemconfirm.jsp
@@ -0,0 +1,116 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>새소식작성</title>
+</head>
+<style>
+.itemImage {
+    height: 300px;
+    background-repeat: no-repeat;
+    background-position: 50%;
+    background-size: cover;
+}
+.itemImageBack {
+	background : rgba(0,0,0,.5);
+    /* background: linear-gradient(135deg,rgba(0,0,0,.7) 20%,transparent); */
+    height: 300px;
+}
+.itemTextBox {
+    padding-left: 3rem;
+    padding-right: 3rem;
+    padding-top: 3rem;
+    line-height: .5;
+}
+.itemname {
+	text-decoration: none;
+	font-weight: 700;
+    font-size: 1.7em;
+    color: #fff;
+    line-height: 1.4;
+    text-shadow: 0 2px 2px rgba(0,0,0,.2);
+}
+.itemdetail{
+    font-size: 1em;
+    line-height: 1.5;
+    color: #fff;
+    padding-top: .4em;
+    padding-left: 3rem;
+    text-shadow: 0 2px 2px rgba(0,0,0,.2);
+}
+.menubox{
+    position: relative;
+    background: #fff;
+    box-shadow: none;
+    margin: 1rem 0;
+    padding: 1em;
+    border-radius: .28571429rem;
+    border: 1px solid rgba(0,0,0,.1);
+}
+</style>
+<body>
+	<jsp:include page="/WEB-INF/views/common/menubar.jsp" flush="true" />
+	<div style="background-color:#F6F5F5">
+	<div class="row" style="position: relative">
+		<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+			<div id=itemImage class="itemImage" style="background-image: url(images/logo.png">
+				<div class="itemImageBack">
+					<div class="itemTextBox">
+						<a class="itemname">FunFund 관리자</a>
+						<br><br>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+		<div class="row" style="top: -7.0rem; position: relative;">
+			<div class="col-xs-1 col-sm-1 col-md-2 col-lg-2"></div>
+			<div class="col-xs-10 col-sm-10 col-md-8 col-lg-8 menubox">
+				<div class="middle-submenu">
+					<ul class="nav nav-pills middle-submenu"
+						style="width: 900px; align: center;">
+						<li id="info1" class="active-active"><a href="#">프로젝트 관리</a></li>
+						<li id="info2"><a href="#">펀딩금액 관리</a></li>
+						<li id="info3"><a href="#">funfund 현황</a></li>
+					</ul>
+				</div>
+			</div>
+			<div class="col-xs-1 col-sm-1 col-md-2 col-lg-2"></div>
+		</div>
+	</div>
+	<div class="container">
+  <h2>Hover Rows</h2>
+  <p>The .table-hover class enables a hover state on table rows:</p>            
+  <table class="table table-hover">
+    <thead>
+      <tr>
+        <th>Firstname</th>
+        <th>Lastname</th>
+        <th>Email</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>John</td>
+        <td>Doe</td>
+        <td>john@example.com</td>
+      </tr>
+      <tr>
+        <td>Mary</td>
+        <td>Moe</td>
+        <td>mary@example.com</td>
+      </tr>
+      <tr>
+        <td>July</td>
+        <td>Dooley</td>
+        <td>july@example.com</td>
+      </tr>
+    </tbody>
+  </table>
+</div>
+</body>
+</html>
